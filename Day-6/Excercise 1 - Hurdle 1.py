da082023b5911fc0ec1ffb5683a582c2c9c07315
arpitsengar99 <arpitsengar99@gmail.com>
2022-11-27 13:12:53 +0530

    Day 6 completed

---
+++ Day-6/Excercise 1 - Hurdle 1.py
@@ -0,0 +1,18 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+for step in range(6):
+    jump()
---
+++ Day-6/Excercise 3 - Hurdle 3.py
@@ -0,0 +1,24 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+    
+   
+
+    
