52021536d00616db53d70df990d27839a8735c4c
arpitsengar99 <arpitsengar99@gmail.com>
2023-04-27 19:54:11 +0530

    Day 81 completed

---
+++ Days-81-to-90/Day-81/morse_code_convertor/main.py
@@ -0,0 +1,31 @@
+from art import ASCII_ART
+
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-', ' ': '/'}
+
+print(ASCII_ART)
+
+
+def convertor(string):
+    letter_list = [i for i in string]
+    final = [MORSE_CODE_DICT[letter] for letter in letter_list]
+
+    return final
+
+
+while True:
+    user_input = str(input("ENTER YOUR STRING: ")).upper()
+    print(*convertor(user_input), "\n")
